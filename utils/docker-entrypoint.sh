f46335ba353a61166f3a74c2a4447c7b7172f8dc
Kuba Kafik <jakub@kaflik.pl>
2025-04-11 12:41:32 +0000

    extend docker-entrypoint.sh

--- utils/docker-entrypoint.sh
+++ utils/docker-entrypoint.sh
@@ -3,18 +3,25 @@ set -e
 
 if [ -z "${OM_DATUM_LAT}" ]; then
   export OM_DATUM_LAT=30.0
+
+  echo "OM_DATUM_LAT not set, using default value: ${OM_DATUM_LAT}"
 fi
 
 if [ -z "${OM_DATUM_LONG}" ]; then
   export OM_DATUM_LONG=0.5
+
+  echo "OM_DATUM_LONG not set, using default value: ${OM_DATUM_LONG}"
 fi
 
 if [ -z "${OM_MAP_PATH}" ]; then
   export OM_MAP_PATH=${WORKSPACE}/map/json
+
+  echo "OM_MAP_PATH not set, using default value: ${OM_MAP_PATH}"
 fi
 
 if [ ! -f "${OM_MAP_PATH}" ]; then
-  touch ${OM_MAP_PATH}
+  echo "{}" > ${OM_MAP_PATH} # empty json file
+  echo "Created empty map file at ${OM_MAP_PATH}"
 fi
 
 # Source ROS environment
