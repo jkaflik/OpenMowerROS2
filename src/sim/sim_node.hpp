dca139a6ff1e6a6f09dc7ae4a2c8248d04db63e8
Kuba Kaflik <jakub@kaflik.pl>
2025-04-16 00:11:37 +0200

    nav2 Jazzy fixes  (#29)
    * nav2 a bit better
    
    * make sim charging more reliable

--- src/sim/sim_node.hpp
+++ src/sim/sim_node.hpp
@@ -6,23 +6,26 @@
 #include <geometry_msgs/msg/pose.hpp>
 #include <std_msgs/msg/float32.hpp>
 #include "open_mower_next/msg/map.hpp"
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2/time.h>
 
-namespace open_mower_next::sim {
+namespace open_mower_next::sim
+{
 
-class SimNode final : public rclcpp::Node {
+class SimNode final : public rclcpp::Node
+{
 public:
-  explicit SimNode(const rclcpp::NodeOptions &options);
+  explicit SimNode(const rclcpp::NodeOptions& options);
 
   ~SimNode() override = default;
 
 private:
-  msg::Map::SharedPtr map_;
-  geometry_msgs::msg::PoseStamped::SharedPtr model_baselink_pose_;
-  geometry_msgs::msg::PoseStamped::SharedPtr docking_station_pose_;
-
-  rclcpp::Subscription<msg::Map>::SharedPtr map_subscriber_;
-  rclcpp::Subscription<tf2_msgs::msg::TFMessage>::SharedPtr model_tf_subscriber_;
   rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr docking_station_pose_subscriber_;
+  std::shared_ptr<geometry_msgs::msg::PoseStamped> docking_station_pose_;
+  // Docking station pose offset parameters
+  double docking_station_offset_x_;
+  double docking_station_offset_y_;
 
   std_msgs::msg::Bool charger_present_msg_;
   sensor_msgs::msg::BatteryState battery_state_msg_;
@@ -41,12 +44,15 @@ private:
   double battery_state_voltage_charge_per_second_;
   rclcpp::Time last_battery_voltage_update_;
 
+  // TF2 buffer and listener
+  std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+
   void mapCallback(msg::Map::SharedPtr msg);
-  void modelTfCallback(tf2_msgs::msg::TFMessage::SharedPtr msg);
   void dockingStationPoseCallback(geometry_msgs::msg::Pose::SharedPtr msg);
   void chargerPresentSimulationCallback();
   void batteryStateSimulationCallback();
 
   bool isInDockingStation();
 };
-};
+};  // namespace open_mower_next::sim
